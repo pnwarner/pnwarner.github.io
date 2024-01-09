48f585fea08a839cbb7b8b131d78717a5c719eda
Patrick Warner <patrick.warner@paradoxresearch.net>
2024-01-09 11:59:38 -0800

    update mxblog script index page

--- script/mxblog/start_app.js
+++ script/mxblog/start_app.js
@@ -1,4 +1,5 @@
 document.body.onload = function() {
-    const blog_display_object = new Article_Reader_Display_Object("",["","",0,0],["terraform","","",""]);
+    //const blog_display_object = new Article_Reader_Display_Object("",["","",0,0],["terraform","","",""]);
+    const blog_display_object = new Article_Reader_Display_Object("",["https://pnwarner.github.io/media/json/mxblog/test-mxblog/test-blog/pr-dev-blog.json","blog",0,0],["terraform","","",""]);
     const new_blog = new Article_Reader(blog_display_object, {});
 }
