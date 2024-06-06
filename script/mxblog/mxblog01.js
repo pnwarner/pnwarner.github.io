96a9d307f82a2409e2d48ed3b1c67e79c9007ae4
Patrick Warner <patrick.warner@paradoxresearch.net>
2024-06-05 22:33:54 -0700

    Change blog display

--- script/mxblog/mxblog01.js
+++ script/mxblog/mxblog01.js
@@ -6,7 +6,7 @@ const app_script_list = [
     'https://pnwarner.github.io/script/mxblog/js/article_editor.js',
     'https://pnwarner.github.io/script/mxblog/js/article_reader_display_object.js',
     'https://pnwarner.github.io/script/mxblog/js/article_editor_display_object.js',
-    'https://pnwarner.github.io/script/mxblog/js/start_app.js'
+    'https://pnwarner.github.io/script/mxblog/js/start_app_blog.js'
 ];
 
 function add_script_element(script_addr){
