5d5dc7219a2cf06737bcbe87812a5858fa4bd48a
Patrick Warner <patrick.warner@paradoxresearch.net>
2024-06-05 22:21:45 -0700

    feat: add new blog section to site

---
+++ script/mxblog/dist/mxblog.dev.js
@@ -0,0 +1,18 @@
+"use strict";
+
+var app_script_list = ['https://pnwarner.github.io/script/mxblog/js/article_class.js', 'https://pnwarner.github.io/script/mxblog/js/blog_class.js', 'https://pnwarner.github.io/script/mxblog/js/xblog_class.js', 'https://pnwarner.github.io/script/mxblog/js/article_reader.js', 'https://pnwarner.github.io/script/mxblog/js/article_editor.js', 'https://pnwarner.github.io/script/mxblog/js/article_reader_display_object.js', 'https://pnwarner.github.io/script/mxblog/js/article_editor_display_object.js', 'https://pnwarner.github.io/script/mxblog/js/start_app_blog.js'];
+
+function add_script_element(script_addr) {
+  var new_script_element = document.createElement('script');
+  new_script_element.type = 'text/javascript';
+  new_script_element.src = script_addr;
+  document.body.appendChild(new_script_element);
+}
+
+function init() {
+  app_script_list.forEach(function (script_addr) {
+    add_script_element(script_addr);
+  });
+}
+
+init();
